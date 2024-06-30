a02822839daa644e8105978bcf01957096db5907
GK1009 <117459753+gk10092004@users.noreply.github.com>
2024-06-30 17:44:58 +0530

    cors updates

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ["https://sm-auth-frontend.vercel.app"],
-        methods:["POST","GET"],
-        credentials: true
-    }
-))
+app.use(cors({ origin: 'https://sm-auth-frontend.vercel.app' }));
 const userRoute = require('./routes/userRoute');
 app.use('/api',userRoute);
 
