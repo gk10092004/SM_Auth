263769fb6aa925c7efc514396bfdefaf1fc08671
GK1009 <117459753+gk10092004@users.noreply.github.com>
2024-06-30 17:26:41 +0530

    cors updates

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors(
+    {
+        origin: [""],
+        methods:["POST","GET"],
+        credentials: true
+    }
+))
 const userRoute = require('./routes/userRoute');
 app.use('/api',userRoute);
 
