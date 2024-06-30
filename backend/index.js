583d41360f5c0939f89ea8bcc25b73dca773c780
GK1009 <117459753+gk10092004@users.noreply.github.com>
2024-06-30 17:31:36 +0530

    cors origin add

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["http://localhost:5173/"],
+        origin: ["https://sm-auth-frontend.vercel.app/"],
         methods:["POST","GET"],
         credentials: true
     }
